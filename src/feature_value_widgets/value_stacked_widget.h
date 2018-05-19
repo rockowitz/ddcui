4292ec5370e49d9f5234bbfb6dac38daad2b455d
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2018-05-19 00:51:42 -0400

    value_stacked_widget.h: cosmetic

--- src/feature_value_widgets/value_stacked_widget.h
+++ src/feature_value_widgets/value_stacked_widget.h
@@ -37,54 +37,44 @@
 
 class QWidget;
 class QPaintEvent;
-// class QStackedWidget;
 
-class ValueStackedWidget :  public QStackedWidget // public QWidget      // public ValueBaseWidget
-     //  , public QStackedWidget
-      // , public SimpleFeatureValueSubject
+class ValueStackedWidget :  public QStackedWidget
 {
    Q_OBJECT
 
 public:
     explicit ValueStackedWidget(QWidget *parent = nullptr);
 
-    void     setFeatureValue(const FeatureValue &fv) ;
-    void     setCurrentValue(uint16_t newval) ;
-    uint16_t getCurrentValue() ;
+    void     setFeatureValue(const FeatureValue &fv);
+    void     setCurrentValue(uint16_t newval);
+    uint16_t getCurrentValue();
+    bool     isSimpleNc();
+    void     setNcValuesSource(NcValuesSource newsrc);
 
     // QSize sizeHint() const override;
     // void paintEvent(QPaintEvent *event) override;
 
-    bool isSimpleNc();
-    void setNcValuesSource(NcValuesSource newsrc);
-
 signals:
     void stackedFeatureValueChanged(uint8_t feature_code, uint8_t sh, uint8_t sl);
 
-#ifdef OLD
-signals:
-    // compiler warning: signals may not be declared virtual
-    void featureValueChanged(uint8_t feature_code, uint8_t sh, uint8_t sl);
-#endif
-
 private:
-    const char *     _cls;
-    uint8_t                _featureCode;
-    QStackedWidget * _stacked;
-    ValueBaseWidget * _cur_stacked_widget;
+    const char *        _cls;
+    uint8_t             _featureCode;
+    QStackedWidget*     _stacked;
+    int                 _pageno_selected;
+    ValueBaseWidget*    _cur_stacked_widget;
 
-    int  _pageno_std;
-    int  _pageno_cont;
-    int  _pageno_nc;
-    int  _pageno_reset;
-    int  _pageno_2button;
-    int  _pageno_selected;
+    int                 _pageno_std;
+    int                 _pageno_cont;
+    int                 _pageno_nc;
+    int                 _pageno_reset;
+    int                 _pageno_2button;
 
-    ValueContWidget    * _contWidget;
-    ValueNcWidget      * _ncWidget;
-    ValueResetWidget   * _resetWidget;
-    Value2ButtonWidget * _2ButtonWidget;
-    ValueStdWidget     * _stdWidget;
+    ValueStdWidget*     _stdWidget;
+    ValueContWidget*    _contWidget;
+    ValueNcWidget*      _ncWidget;
+    ValueResetWidget*   _resetWidget;
+    Value2ButtonWidget* _2ButtonWidget;
 
 private slots:
     void onContainedWidgetChanged(uint8_t feature_code, uint8_t sh, uint8_t sl);
@@ -92,3 +82,5 @@ private slots:
 };
 
 #endif // VALUE_STACKED_WIDGET_H
+
+
