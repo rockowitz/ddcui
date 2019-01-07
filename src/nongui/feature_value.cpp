282847265d61e14e36d15efe0891f88c25a92bf3
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-01-06 20:43:51 -0500

    feature_value.cpp: copyright notice cleanup

--- src/nongui/feature_value.cpp
+++ src/nongui/feature_value.cpp
@@ -1,7 +1,7 @@
 /* feature_value.cpp */
 
 // Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
-/// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 // #include <QMetaType>
 
