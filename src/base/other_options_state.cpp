45e4e79505b8f90ca14b71940215a9d04488b2e0
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-05-26 23:01:33 -0400

    other_options_state.cpp/h: - ncValuesSoruceName():   - return const char * instead of char *

--- src/base/other_options_state.cpp
+++ src/base/other_options_state.cpp
@@ -1,11 +1,11 @@
 // other_options_state.cpp
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2019 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "other_options_state.h"
 
-char *
+const char *
 ncValuesSourceName(NcValuesSource source)
 {
      char * result = NULL;
--- src/base/other_options_state.h
+++ src/base/other_options_state.h
@@ -1,6 +1,6 @@
 // other_options_state.h
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2019 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef OTHER_OPTIONS_STATE_H_
@@ -24,6 +24,6 @@ public:
    NcValuesSource ncValuesSource = DefaultNcValuesSource;
 };
 
-char * ncValuesSourceName(NcValuesSource source) ;
+const char * ncValuesSourceName(NcValuesSource source) ;
 
 #endif /* OPTIONS_STATE_H_ */
