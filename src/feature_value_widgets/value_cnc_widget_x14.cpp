819703a67dd2054c65b96ca64f238b9981eda195
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-10-08 00:18:46 -0400

    value_cnc_widget_X14.cpp/h: - New class subclassed from ValueNcWidget(), specifically for use  with feature x14 which in later MCCS specs uses the MH byte  to indicate tolerances
     CMakeLists.txt:
     - Include the new files to be built

---
+++ src/feature_value_widgets/value_cnc_widget_x14.cpp
@@ -0,0 +1,171 @@
+// value_cnc_widget_x14.cpp
+
+// Copyright (C) 2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "feature_value_widgets/value_cnc_widget_x14.h"
+#include "feature_value_widgets/value_nc_widget.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <iostream>
+
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QLayout>
+#include <QtWidgets/QLabel>
+
+#include <ddcutil_c_api.h>
+
+#include "base/ddca_utils.h"
+#include "base/ddcui_globals.h"
+#include "base/debug_utils.h"
+#include "base/global_state.h"
+#include "base/other_options_state.h"
+
+#include "nongui/feature_base_model.h"
+
+
+
+
+ValueCncWidgetX14::ValueCncWidgetX14(QWidget *parent):
+        ValueNcWidget(parent)
+{
+    _cls = strdup(metaObject()->className());
+    // PRINTFCM("Starting." );
+
+#ifdef REF
+   QFont nonMonoFont8;
+   nonMonoFont8.setPointSize(8);
+
+   QFont nonMonoFont9;
+   nonMonoFont9.setPointSize(9);
+
+    _cb = new QComboBox();
+
+    QSizePolicy* sizePolicy = new QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    sizePolicy->setControlType(QSizePolicy::ComboBox);
+    _cb->setSizePolicy(*sizePolicy);
+    _cb->setFont(nonMonoFont8);
+    _cb->setMaximumHeight(20);
+    // whatever the size, large or small, causes big gap between RW and feature value
+    _cb->setMaximumWidth(320);
+    // _cb->setFrameStyle(QFrame::Sunken | QFrame::Panel);   // not a method
+    _cb->setStyleSheet("background-color:white;color:black;");
+
+#ifdef APPLY_CANCEL
+    if (useApplyCancel) {
+       sizePolicy->setControlType(QSizePolicy::PushButton);
+       _applyButton  = new QPushButton("Apply");
+       _cancelButton = new QPushButton("Cancel");
+       _applyButton->setMaximumSize(55,20);
+       _applyButton->setSizePolicy(*sizePolicy);
+       _applyButton->setFont(nonMonoFont9);
+       _cancelButton->setMaximumSize(55,20);
+       _cancelButton->setSizePolicy(*sizePolicy);
+       _cancelButton->setFont(nonMonoFont9);
+    }
+#endif
+
+    QHBoxLayout * layout = new QHBoxLayout();
+    layout->addSpacing(5);
+    layout->addWidget(_cb);
+    layout->addStretch(1);
+#ifdef APPLY_CANCEL
+    if (useApplyCancel) {
+       layout->addWidget(_applyButton);
+       layout->addWidget(_cancelButton);
+    }
+    else {
+       layout->addSpacing(10);
+    }
+#else
+    layout->addSpacing(10);
+#endif
+    layout->setContentsMargins(0,0,0,0);
+    setLayout(layout);
+
+    if (debugLayout) {
+        if (!dimensionReportShown) {
+            PRINTFCM("combobox dimensions");
+            reportWidgetDimensions(_cb, _cls, __func__);
+            PRINTFCM("ValueNcWidget dimensions");
+            reportWidgetDimensions(this, _cls, __func__);
+            dimensionReportShown = true;
+        }
+        this->setStyleSheet("background-color:cyan;");
+    }
+
+    QObject::connect(_cb,  SIGNAL(activated(int)),
+                     this, SLOT(combobox_activated(int)) );
+
+#endif
+
+    _mhlabel = new QLabel("Tolerance:");
+    _mhdesc  = new QLabel();
+
+
+    delete _layout;
+
+    _layout = new QHBoxLayout();
+    _layout->addSpacing(5);
+    _layout->addWidget(_cb);
+    _layout->addStretch(1);
+
+#ifdef UNUSED
+    _layout->addWidget(_mhlabel);
+#endif
+    _layout->addWidget(_mhdesc);
+    _layout->addStretch(2);
+
+ #ifdef APPLY_CANCEL
+     if (useApplyCancel) {
+        _layout->addWidget(_applyButton);
+        _layout->addWidget(_cancelButton);
+     }
+     else {
+        _layout->addSpacing(10);
+     }
+ #else
+     _layout->addSpacing(10);
+ #endif
+     _layout->setContentsMargins(0,0,0,0);
+     setLayout(_layout);
+}
+
+
+void     ValueCncWidgetX14::setFeatureValue(const FeatureValue &fv) {
+   ValueNcWidget::setFeatureValue(fv);
+
+   const char * mh_msg = "";
+    // how to pull in x14_color_preset_tolerances from vcp_feature_codes.c instead,
+   // or the formatted MH value?
+
+   // TODO, either define ddcui versions of vcp_version_le(), etc, or put into api
+   DDCA_MCCS_Version_Spec vspec = fv.vspec();
+   if ( (vspec.major == 2 && vspec.minor >= 2) || vspec.major == 3) {
+   char buf0[100];
+   if (_mh == 0x00)
+      mh_msg = "No tolerance specified. (mh=0x00)";
+   else if (_mh >= 0x0b) {
+      snprintf(buf0, 100, "Invalid tolerance. (mh=0x%02x)", _mh);
+      mh_msg = buf0;
+   }
+   else {
+      snprintf(buf0, 100, "Tolerance: %d%%  (MH=0x%02x)", _mh, _mh);
+      mh_msg = buf0;
+   }
+   }
+
+   // QString& mhtext(mh_msg);
+   _mhdesc->setText(mh_msg);
+}
+
+// void     ValueCncWidgetX14::setCurrentValue(uint16_t newval) {
+// }
+
+
+
+
+
---
+++ src/feature_value_widgets/value_cnc_widget_x14.h
@@ -0,0 +1,58 @@
+// value_cnc_widget_x14.h
+
+// Copyright (C) 2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef VALUE_CNC_WIDGET_X14_H_
+#define VALUE_CNC_WIDGET_X14_H_
+
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QLabel>
+
+#include "config.h"
+#include "base/ddca_utils.h"
+#include "base/other_options_state.h"
+#include "feature_value_widgets/value_base_widget.h"
+
+#include "feature_value_widgets/value_nc_widget.h"
+
+
+class ValueCncWidgetX14 : public ValueNcWidget
+{
+    Q_OBJECT
+
+public:
+    ValueCncWidgetX14(QWidget *parent = nullptr);
+
+    void     setFeatureValue(const FeatureValue &fv) override;
+    // void     setCurrentValue(uint16_t newval) override;
+    // uint16_t getCurrentValue() override;
+
+    // int findItem(uint8_t sl_value);
+    // Local_Feature_Value_Table * getComboBoxEntries(NcValuesSource mode);
+    // void loadComboBox(NcValuesSource mode);
+    // void reloadComboBox(NcValuesSource newSource);
+
+private:
+    QLabel * _mhdesc;
+    QLabel * _mhlabel;
+
+#ifdef NO
+    QComboBox *    _cb;
+#ifdef APPLY_CANCEL
+    QPushButton *  _applyButton;
+    QPushButton *  _cancelButton;
+#endif
+    NcValuesSource _curNcValuesSource = OtherOptionsState::DefaultNcValuesSource;
+    bool           _guiChange = false;
+
+private slots:
+    void combobox_activated(int index);
+#endif
+};
+
+
+
+#endif /* VALUE_CNC_WIDGET_X14_H_ */
