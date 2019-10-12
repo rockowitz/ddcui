10af8963be9455a368f9de0856c8367135f3ab35
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-10-12 03:43:48 -0400

    enhanced_slider.cpp/h - new files - implement a subclass of QSlider that allows for only moving   the slider when the control key is pressed

---
+++ src/base/enhanced_slider.cpp
@@ -0,0 +1,101 @@
+/** @file enhanced_slider.cpp
+ */
+
+// Copyright (C) 2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <QtGui/QKeyEvent>
+#include <QtGui/QMouseEvent>
+#include <QtWidgets/QSlider>
+
+#include "c_util/string_util.h"
+
+#include "base/ddcui_globals.h"
+#include "base/debug_utils.h"
+
+#include "enhanced_slider.h"
+
+EnhancedSlider::EnhancedSlider(QWidget * parent)
+        : QSlider(parent)
+        , _ctrl_key_is_pressed(false)
+        , _ctrl_key_required(true)
+{
+   _cls = strdup(metaObject()->className());
+}
+
+EnhancedSlider::EnhancedSlider(Qt::Orientation orientation, QWidget * parent)
+  : QSlider(orientation, parent)
+  , _ctrl_key_is_pressed(false)
+  , _ctrl_key_required(true)
+{
+   _cls = strdup(metaObject()->className());
+}
+
+// EnhancedSlider::~EnhancedSlider() {
+//    ~QSlider::QSlider();
+// }
+
+// This is wrong.  Need to set at class level, not for individual instances
+void EnhancedSlider::require_control_key(bool onoff) {
+   _ctrl_key_required = onoff;
+}
+
+
+static void dbgrptQKeyEvent(QKeyEvent * event) {
+   printf("  key:  %d\n", event->key());
+   printf("  modifiers: 0x%x\n", (unsigned int) event->modifiers());
+   printf("  nativeModifiers: 0x%08x\n", event->nativeModifiers());
+   printf("  nativeScanCode:  0x%08x\n", event->nativeScanCode());
+}
+
+
+void EnhancedSlider::mouseMoveEvent(QMouseEvent *    ev)
+{
+   // PRINTFCM("Starting, _ctrl_key_is_pressed = %s, _ctrl_key_required = %s",
+   //          SBOOL(_ctrl_key_is_pressed), SBOOL(_ctrl_key_required));
+   if (_ctrl_key_is_pressed || !_ctrl_key_required)
+      QSlider::mouseMoveEvent(ev);
+
+   // ev->ignore();    // needed?
+}
+
+void EnhancedSlider::mousePressEvent(QMouseEvent *   ev)
+{
+   // PRINTFCM("Starting, _ctrl_key_is_pressed = %s, _ctrl_key_required = %s",
+   //          SBOOL(_ctrl_key_is_pressed), SBOOL(_ctrl_key_required));
+   if (_ctrl_key_is_pressed|| !_ctrl_key_required)
+      QSlider::mousePressEvent(ev);
+
+   // ev->ignore();    // needed?
+}
+
+void EnhancedSlider::mouseReleaseEvent(QMouseEvent * ev)
+{
+   // PRINTFCM("Starting, _ctrl_key_is_pressed = %s, _ctrl_key_required = %s",
+   //          SBOOL(_ctrl_key_is_pressed), SBOOL(_ctrl_key_required));
+   if (_ctrl_key_is_pressed|| !_ctrl_key_required)
+      QSlider::mouseReleaseEvent(ev);
+
+   // ev->ignore();    // needed?
+}
+
+void EnhancedSlider::keyPressEvent(QKeyEvent *   ev)
+{
+   // PRINTFCM("Starting");
+   // dbgrptQKeyEvent(ev);
+   if (ev->key() == Qt::Key_Control)    // 68
+      _ctrl_key_is_pressed = true;
+   QSlider::keyPressEvent(ev);
+
+   ev->ignore();    // needed?
+}
+
+void EnhancedSlider::keyReleaseEvent(QKeyEvent *   ev)
+{
+   // PRINTFCM("Starting");
+   // dbgrptQKeyEvent(ev);
+   if (ev->key() == Qt::Key_Control)    // 68
+      _ctrl_key_is_pressed = false;
+   QSlider::keyPressEvent(ev);
+   ev->ignore();    // needed?
+}
---
+++ src/base/enhanced_slider.h
@@ -0,0 +1,46 @@
+/** @file enhanced_slider.h
+ *
+ *  Subclass of QSlider that only moves the slider when the control
+ *  key is pressed.
+ */
+
+// Copyright (C) 2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <QtWidgets/QWidget>
+
+class QSlider;
+class QMouseEvent;
+class QKeyEvent;
+
+#ifndef ENHANCED_SLIDER_H_
+#define ENHANCED_SLIDER_H_
+
+class EnhancedSlider  : public QSlider
+{
+   Q_OBJECT
+
+public:
+   EnhancedSlider(Qt::Orientation orientation, QWidget * parent = nullptr);
+   explicit EnhancedSlider(QWidget *parent = nullptr);
+
+   // ~EnhancedSlider();
+
+   void require_control_key(bool onoff);
+
+protected:
+   void mouseMoveEvent(QMouseEvent *    ev) override;
+   void mousePressEvent(QMouseEvent *   ev) override;
+   void mouseReleaseEvent(QMouseEvent * ev) override;
+
+   void keyPressEvent(QKeyEvent *    ev) override;
+   void keyReleaseEvent(QKeyEvent *   ev) override;
+
+private:
+   char *   _cls;
+   bool     _ctrl_key_is_pressed;
+   bool     _ctrl_key_required;
+
+};
+
+#endif /* ENHANCED_SLIDER_H_ */
