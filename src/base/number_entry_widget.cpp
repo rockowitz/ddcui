dede7c45d009684ffcb1d38e65353d4579b2e10b
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-10-15 06:39:40 -0400

    number_entry_widget.cpp/h: - work in progress

--- src/base/number_entry_widget.cpp
+++ src/base/number_entry_widget.cpp
@@ -1,6 +1,6 @@
 // number_entry_widget.cpp
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2019 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 
@@ -13,40 +13,60 @@
 
 #include "base/ddcui_globals.h"
 #include "base/debug_utils.h"
+#include "base/hex_number_validator.h"
 
 #include "number_entry_widget.h"
  
-
+// initializes _widgetState to StateOldValid
 NumberEntryWidget::NumberEntryWidget(
       uint8_t bytect,
       QFont   font,
       int     style,
       QSize   size,
       QWidget *parent)
-    : QLineEdit(nullptr)
-    , _bytect(bytect)
+   : QLineEdit(nullptr)
+   , _bytect(bytect)
+   , _curval(0)
+   , _widgetState(NumberEntryWidget::StateOldValid)
 {
-    // how ensure bytect between 1 and 4?
+   _cls = strdup(metaObject()->className());
+    // TODO: use an enumb instead of bytect
+   assert(bytect == 1 || bytect == 2);    // could allow 4, just more cases in switch stmts etc.
 
-    QString rxs;
+    // *** Constrain what character strings are accepted
 
-    switch(bytect) {
-    case 1:  rxs = QString("^[xX][A-Fa-f0-9](1)$");    break;
+ /* All three methods work.  All provide that at most 2 hexacecimal characters
+  * are entered, so the value shown is always valid.  true for 1, 2, what about 3?
+  * All three have the same deviciency.
+  */
 
-    // case 2:
-    // case 3:
-    // case 4:
-    default:
-       // how to fail?
-    throw std::runtime_error("Invalid bytect");
-    }
-
+#ifdef METHOD1
+   // Method 1, a regular expression validator
+      // Unlike the custom validator, does not even allow invalid chars to be entered
+    // same problem as methods 1 and 2, only backspace works
+    QString rxs = QString("^[A-Fa-f0-9]{%1}$").arg(2*bytect,1,10,QLatin1Char('0')) ;
     QRegExp rx = QRegExp(rxs);
-
     setValidator( new QRegExpValidator(rx));
+#endif
 
-    setMaxLength( bytect+1);
+// #ifdef METHOND2
+    // Method 2: use Input Mask
+    // also allows only valid hex characters to be entered.
+    // but hard to change data, have to click on right then basckspace to be able to enter text
+    QString hexByteInputMask("HH");
+    setInputMask(hexByteInputMask);
+// #endif
 
+#ifdef METHOD3
+    // similar problem, delete key never works, must position cursor to right of a digit, then
+    // can use backspace
+    // now it's blocking all input???
+    // Method 3: Custom validator
+    QValidator* v = new HexNumberValidator();
+    setValidator(new HexNumberValidator());
+#endif
+
+    setMaxLength( 2*bytect);
     setFont(font);
     // setFrameStyle(style); ???
     setFixedSize(size);
@@ -55,7 +75,151 @@ NumberEntryWidget::NumberEntryWidget(
     if (debugLayout)
        setStyleSheet("background-color:orange;");
 
+    connect(this, &NumberEntryWidget::textEdited,  this, &NumberEntryWidget::onTextEdited);
 }
 
 
+uint16_t
+NumberEntryWidget::getExistingValue()
+{
+   bool debug = false;
+   PRINTFCMF(debug, "widget = %s, returning: x%04x", qs2s(this->objectName()), _curval);
+   return _curval;
+}
 
+uint16_t
+NumberEntryWidget::getNewValue()
+{
+   bool debug = false;
+   PRINTFCMF(debug, "widget = %s, returning: x%04x", qs2s(this->objectName()), _valueEntered);
+   return _valueEntered;
+}
+
+// sets the current value
+// changes state to StateOldValue
+void
+NumberEntryWidget::setValue(uint16_t newval)
+{
+   bool debug = false;
+   PRINTFCMF(debug, "widget = %s, newval = x%04x", qs2s(this->objectName()), newval);
+
+   // Set text value in widget
+   int textLength = 2 * _bytect;
+   QString sval = QString("%1").arg(newval,textLength,16,QLatin1Char('0'));
+   setText(sval);
+
+   _curval = newval;
+   _widgetState = NumberEntryWidget::StateOldValid;
+}
+
+
+NumberEntryWidget::States
+NumberEntryWidget::getState()
+{
+   return _widgetState;
+}
+
+// if widgestate = OldState, do nothing
+// o.w. sets the displayed value to curval
+//      set state to OldState
+void
+NumberEntryWidget::reset()
+{
+   bool debug = false;
+   PRINTFCMF(debug, "Starting. widget = %s, _curState = %d", qs2s(this->objectName()),_widgetState);
+   if (_widgetState != NumberEntryWidget::StateOldValid ) {
+      int textLength = 2*_bytect;
+      _valueEntered = 0;
+      setText( QString("%1").arg(_curval,textLength,16,QLatin1Char('0')) );
+      _widgetState = NumberEntryWidget::StateOldValid;
+   }
+   PRINTFCMF(debug, "Done. _curState = %d", _widgetState);
+}
+      
+
+#ifdef OLD
+void
+NumberEntryWidget::onTextEdited(const QString &text) {
+   PRINTFCMF(true, "Starting. text=%s", text.toLatin1().data());
+   bool ok = false;
+   if (text.length() > 0) {
+
+
+      int newval = text.toInt(&ok, 16);
+      if (ok) {          // should already have been checked
+         _curval = newval;
+         PRINTFCMF(true, "About to emit, _curval = newval = 0x%02x", newval);
+
+         emit NumberEntryWidget::valueChanged8( newval & 0xff);
+      }
+      else {
+         PRINTFCM("Not a number: %s", qs2s(text));
+      }
+   }
+   PRINTFCM("emitting NumberEntryWidet::isValidValue, _fieldNumber = %d, ok=%s",
+            _fieldNumber, sbool(ok) );
+   emit NumberEntryWidget::isValidValue(_fieldNumber, ok);
+}
+#endif
+
+
+// sets newval to the int value of the tex text
+// if value shown has length 0, sets state to Invalid
+// if value shown (valueEntered)  == oldval, sets state to OldValid
+//  o.w.                sets state to NewValid
+
+void
+NumberEntryWidget::onTextEdited(const QString &text) {
+   bool debug = false;
+   PRINTFCMF(debug, "Starting. text=%s", text.toLatin1().data());
+
+   NumberEntryWidget::States newState = NumberEntryWidget::StateInvalid;
+   bool ok = false;
+   _valueEntered = 0;
+   if (text.length() > 0) {
+      _valueEntered = text.toInt(&ok, 16);
+      if (ok) {          // should already have been checked
+         if (_curval == _valueEntered)
+            newState = NumberEntryWidget::StateOldValid;
+         else
+            newState = NumberEntryWidget::StateNewValid;
+      }
+      else {
+         PRINTFCM("Not a number: %s", qs2s(text));
+      }
+   }
+
+   if (newState != _widgetState && newState != NumberEntryWidget::StateInvalid)
+   {
+      PRINTFCMF(debug, "About to emit stateChanged, newstate = %d", newState);
+      emit NumberEntryWidget::stateChanged(this, newState);
+   }
+
+   _widgetState = newState;
+   // if (newState ==  NumberEntryWidget::StateNewValid )
+   //    _curval = newval;
+   PRINTFCMF(debug, "Done. setting _widgetState to %d", _widgetState);
+}
+
+
+// For exploring behavior
+
+void
+NumberEntryWidget::focusInEvent(QFocusEvent * event) {
+   // PRINTFCM("Executing, _curState = %d", _widgetState);
+}
+
+void
+NumberEntryWidget::focusOutEvent(QFocusEvent * event) {
+   // PRINTFCM("Executing, _curState = %d", _widgetState);
+}
+
+void
+NumberEntryWidget::mousePressEvent(QMouseEvent * event) {
+   // PRINTFCM("Executing, _curState = %d", _widgetState);
+}
+
+void
+NumberEntryWidget::mouseReleaseEvent(QMouseEvent * event) {
+   // PRINTFCM("Executing, _curState = %d", _widgetState);
+}
--- src/base/number_entry_widget.h
+++ src/base/number_entry_widget.h
@@ -15,20 +15,47 @@ class QLineEdit;
 class NumberEntryWidget : public QLineEdit
 {
     Q_OBJECT
+    // Q_ENUMS(States)
 
 public:
+    enum States {StateOldValid, StateInvalid, StateNewValid};
+
     NumberEntryWidget(
-          uint8_t bytect,
-          QFont   font,
-          int     style,
-          QSize   size,
+          uint8_t  bytect,
+          QFont    font,
+          int      style,
+          QSize    size,
           QWidget *parent = nullptr);
 
+    void setValue(uint16_t byteval);
+    NumberEntryWidget::States getState();
+    uint16_t getExistingValue();
+    uint16_t getNewValue();
+    void reset();
+
+protected:
+    void focusInEvent(QFocusEvent * event)      override;
+    void focusOutEvent(QFocusEvent * event)     override;
+    void mousePressEvent(QMouseEvent * event)   override;
+    void mouseReleaseEvent(QMouseEvent * event) override;
+
+private slots:
+    void onTextEdited(const QString &text);
+
+signals:
+   // void valueChanged8(uint8_t  value);
+   // void valueChanged16(uint16_t value);
+   // void valueChanged32(uint32_t value);
+   // void isValidValue(uint8_t fieldNumber, bool tf);
+   // void setCurrentField(int fieldNumber);
+   void stateChanged(NumberEntryWidget * widget, NumberEntryWidget::States newState);
 
 private:
-    uint8_t _bytect;
-
-
-
+    uint8_t   _bytect;
+    uint16_t  _curval;
+    uint16_t  _valueEntered;  // valid value displayed but not yet accepted
+    char *    _cls;
+    NumberEntryWidget::States
+              _widgetState;
 };
 #endif /* NUMBER_ENTRY_WIDGET_H_ */
