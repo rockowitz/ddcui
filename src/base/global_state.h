a4c3d73763251f0f9a439bda220ac1e387851730
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-10-19 22:59:25 -0400

    global_state.h: - add a pointer to a UserInterfaceOptionsState, along wiht pointer to OtherOptionsState

--- src/base/global_state.h
+++ src/base/global_state.h
@@ -1,12 +1,13 @@
 // global_state.h
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2019 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef GLOBAL_STATE_H_
 #define GLOBAL_STATE_H_
 
 #include "base/other_options_state.h"
+#include "user_interface_options_state.h"
 
 class GlobalState {
 
@@ -14,6 +15,7 @@ public:
    static GlobalState& instance();
 
    OtherOptionsState * _otherOptionsState = NULL;
+   UserInterfaceOptionsState * _uiOptionsState= NULL;
 
 private:
    GlobalState();
