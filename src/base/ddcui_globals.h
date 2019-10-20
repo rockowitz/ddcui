94762d3bf890771f33d26edceeda0be4683e8773
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-10-20 15:02:42 -0400

    ddcui_globals.h: - remove defintions for PRINTFx() macros, no longer used

--- src/base/ddcui_globals.h
+++ src/base/ddcui_globals.h
@@ -41,93 +41,22 @@ bool printftcmf(
       const char * filename,
       const char * format, ...);
 
-
-#ifdef OLD
-#define PRINTFCM(__FMT__, ...) \
-   do { \
-     printf("(%s::%s) " __FMT__ "\n", _cls, __func__, ##__VA_ARGS__); \
-     fflush(stdout); \
-   } while(0)
-#endif
-
-#define PRINTFCM(_FMT, ...) \
-do { \
-   printftcmf(true, _cls, __func__, __LINE__, __FILE__, _FMT, ##__VA_ARGS__); \
-} while(0)
-
-
-#ifdef OLD
-#define PRINTFCMF(__FLAG__, __FMT__, ...) \
-   if (__FLAG__) { \
-      printf("(%s::%s) " __FMT__ "\n", _cls, __func__, ##__VA_ARGS__); \
-      fflush(stdout); \
-   }
-#endif
-
-#define PRINTFCMF(_FLAG, _FMT, ...) \
-do { \
-   printftcmf(_FLAG, _cls. __func__, __LINE__, __FILE__, _FMT, ##__VA_ARGS__); \
-} while(0)
-
-#ifdef OLD
-#define PRINTFTCM(__FMT__, ...) \
-   do { \
-      char thread_prefix[15] = ""; \
-      pid_t tid = syscall(SYS_gettid); \
-     snprintf(thread_prefix, 15, "[%5jd] ", (intmax_t) tid); \
-     printf("%s(%s::%s) " __FMT__ "\n", thread_prefix,  _cls, __func__, ##__VA_ARGS__); \
-     fflush(stdout); \
-   } while(0)
-// variant that shows QtThreadId as well as Linux thread id
-// printf("(%p/%s %s::%s) " __FMT__ "\n",  QThread::currentThreadId(), thread_prefix, _cls, __func__, ##__VA_ARGS__); \
-
-#endif
-
-#define PRINTFTCM(_FMT, ...) \
-   do { \
-      printftcmf(true, _cls, __func__, __LINE__, __FILE__, _FMT, ##__VA_ARGS__); \
-   } while(0)
-
-#ifdef OLD
-#define PRINTFTCMF(__FLAG__, __FMT__, ...) \
-   if (__FLAG__) { \
-      bool dbgtrc_show_thread_id = true;\
-      char thread_prefix[15] = ""; \
-      if (dbgtrc_show_thread_id) { \
-         pid_t tid = syscall(SYS_gettid); \
-         snprintf(thread_prefix, 15, "[%5jd] ", (intmax_t) tid); \
-      } \
-      printf("%s(%s::%s) " __FMT__ "\n",  thread_prefix, _cls, __func__, ##__VA_ARGS__); \
-      fflush(stdout); \
-   }
-#endif
-
-
-#define PRINTFTCMF(_FLAG, _FMT, ...) \
-do { \
-   printftcmf(_FLAG, _cls, __func__, __LINE__, __FILE__, _FMT, ##__VA_ARGS__); \
-} while(0)
-
-
-// new names
 #define TRACE( _FMT, ...) \
 do { \
    printftcmf(true, _cls, __func__, __LINE__, __FILE__, _FMT, ##__VA_ARGS__); \
 } while(0)
-
-
 
 #define TRACEF(_FLAG, _FMT, ...) \
 do { \
    printftcmf(_FLAG, _cls, __func__, __LINE__, __FILE__, _FMT, ##__VA_ARGS__); \
 } while(0)
 
-
-
-
 inline const char * sbool(bool val) { return (val) ? "true" : "false"; }
-
 // #define SBOOL(__v) ( (__v) ? "true" : "false")
+
+inline const char * qs2s(QString qstr) {return qstr.toLatin1().data(); }
+#define QS2S(_qstr) ( _qstr.toLatin1().data() )
+
 
 // #define APPLY_CANCEL
 // #define ALT_MOCK_FEATURES
@@ -136,10 +65,5 @@ inline const char * sbool(bool val) { return (val) ? "true" : "false"; }
 const bool useApplyCancel    = true;
 #endif
 
-
-
-inline const char * qs2s(QString qstr) {return qstr.toLatin1().data(); }
-
-#define QS2S(_qstr) ( _qstr.toLatin1().data() )
 
 #endif // DDCUI_GLOBALS_H
