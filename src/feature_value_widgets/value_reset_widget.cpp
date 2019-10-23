92c15f7ef42b77e3cb55fd97097b500962f0f863
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-10-23 07:09:15 -0400

    value_reset_widget.cpp/h: - move signal/slot connects from layoutWidget() back into constructor - bump copyright year

--- src/feature_value_widgets/value_reset_widget.cpp
+++ src/feature_value_widgets/value_reset_widget.cpp
@@ -1,6 +1,6 @@
 /* value_nc_widget.cpp */
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2019 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "feature_value_widgets/value_reset_widget.h"
@@ -28,43 +28,38 @@ static bool dimensionReportShown = false;
 
 void ValueResetWidget::layoutWidget() {
    QFont nonMonoFont;
-     nonMonoFont.setPointSize(8);
+   nonMonoFont.setPointSize(8);
 
-     _resetButton = new QPushButton("Restore");
-     _resetButton->setMaximumSize(60,20);
+   _resetButton = new QPushButton("Restore");
+   _resetButton->setMaximumSize(60,20);
 
-      QSizePolicy* sizePolicy = new QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-      // _cb->setHorizontalStretch(0);
-      _resetButton->setSizePolicy(*sizePolicy);
-      _resetButton->setFont(nonMonoFont);
-      // _cb->setFrameStyle(QFrame::Sunken | QFrame::Panel);   // not a method
-      // _resetButton->setStyleSheet("background-color:white;");
-      _resetButton->setStyleSheet("background-color:white;color:black;");
+   QSizePolicy* sizePolicy = new QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+   // _cb->setHorizontalStretch(0);
+   _resetButton->setSizePolicy(*sizePolicy);
+   _resetButton->setFont(nonMonoFont);
+   // _cb->setFrameStyle(QFrame::Sunken | QFrame::Panel);   // not a method
+   // _resetButton->setStyleSheet("background-color:white;");
+   _resetButton->setStyleSheet("background-color:white;color:black;");
 
-      QHBoxLayout * layout = new QHBoxLayout();
-      layout->addWidget(_resetButton);
-      layout->addStretch(1);
-      layout->setContentsMargins(0,0,0,0);
-      setLayout(layout);
+   QHBoxLayout * layout = new QHBoxLayout();
+   layout->addWidget(_resetButton);
+   layout->addStretch(1);
+   layout->setContentsMargins(0,0,0,0);
+   setLayout(layout);
 
-      if (debugLayout) {
-         this->setStyleSheet("background-color:cyan;");
-         if (!dimensionReportShown) {
-             // printf("-------------------------------------------->\n"); fflush(stdout);
-             reportWidgetDimensions(_resetButton, _cls, __func__, "ValueResetWidget::_resetButton");
-             reportWidgetDimensions(this, _cls, __func__, "ValueResetWidget");
-             dimensionReportShown = true;
-         }
+   delete(sizePolicy);
+
+   if (debugLayout) {
+      this->setStyleSheet("background-color:cyan;");
+      if (!dimensionReportShown) {
+         // printf("-------------------------------------------->\n"); fflush(stdout);
+         reportWidgetDimensions(_resetButton, _cls, __func__, "ValueResetWidget::_resetButton");
+         reportWidgetDimensions(this, _cls, __func__, "ValueResetWidget");
+         dimensionReportShown = true;
       }
-
-      // QObject::connect(_resetButton,  &QAbstractButton::released),
-      //                  this,          &ValueResetWidget::on_resetButton_pressed );
-
-      QObject::connect(_resetButton,  SIGNAL(released()),
-                       this,          SLOT(  on_resetButton_pressed()) );
-
-      delete(sizePolicy);
+   }
 }
+
 
 ValueResetWidget::ValueResetWidget(QWidget *parent):
         ValueBaseWidget(parent)
@@ -72,6 +67,12 @@ ValueResetWidget::ValueResetWidget(QWidget *parent):
    _cls = strdup(metaObject()->className());
    // TRACE("Starting." );
    layoutWidget();
+
+   // QObject::connect(_resetButton,  &QAbstractButton::released),
+    //                  this,          &ValueResetWidget::on_resetButton_pressed );
+
+    QObject::connect(_resetButton,  SIGNAL(released()),
+                     this,          SLOT(  on_resetButton_pressed()) );
 }
 
 
