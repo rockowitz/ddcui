9992cff8a2bb31cab0faa4f648daaedf5b646fd0
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2019-11-23 05:30:13 -0500

    feature_widget_header.h: - use standard header file organization

--- src/feature_scrollarea/feature_widget_header.h
+++ src/feature_scrollarea/feature_widget_header.h
@@ -1,6 +1,6 @@
 // feature_widget_header.h
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2019 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef FEATURE_WIDGET_HEADER_H_
@@ -22,8 +22,11 @@ protected:
     void resizeEvent(QResizeEvent * event) override;
 
 private:
-   const char * _cls;
-   void layoutWidget();
+    void layoutWidget();
+
+private:     // member variables
+    const char * _cls;
+
 };
 
 #endif /* FEATURE_WIDGET_HEADER_H_ */
