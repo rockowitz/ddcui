597aad23a00a6c53a4eb1a12cad9393a334633df
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2020-01-15 15:10:24 -0500

    value_base_widget.h: - copyright year, comments, formatting

--- src/feature_value_widgets/value_base_widget.h
+++ src/feature_value_widgets/value_base_widget.h
@@ -1,6 +1,6 @@
 /* value_base_widget.h - Common features of all Value_..._Widgets */
 
-// Copyright (C) 2018-2019 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2020 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef VALUE_BASE_WIDGET_H
@@ -19,18 +19,18 @@ class ValueBaseWidget : public QFrame    // was ValueAbstractWidget
 {
     Q_OBJECT
 
-public:
+public:          //methods
     explicit ValueBaseWidget(QWidget *parent = nullptr);
 
     virtual void     setFeatureValue(const FeatureValue &fv); // override;
-    virtual void     setCurrentShSl(uint16_t newval);        // override;
-    virtual uint16_t getCurrentShSl();                       // override;
+    virtual void     setCurrentShSl(uint16_t newval);         // override;
+    virtual uint16_t getCurrentShSl();                        // override;
 
     // QSize sizeHint() const override;   //   needed?
     virtual void     setControlKeyRequired(bool onoff);
     bool             getControlKeyRequired();
 
-protected:
+protected:       // methods
     // void resizeEvent(QResizeEvent * evt) override;
 
 signals:
@@ -55,6 +55,7 @@ protected:      // member variables
     bool                    _controlKeyRequired = false;
     DDCA_Status             _ddcrc = 0;
     GlobalState&            _globalState = GlobalState::instance();
+    int                     _featureValueWidgetHeight = 20;
 
 private:        // member variables
     const char *            _cls;
