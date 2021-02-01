c3fe658578444d37e9bdcdfc343af9d99a74db50
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-02-01 06:02:57 -0500

    bump copyright year in text

--- src/main/mainwindow.cpp
+++ src/main/mainwindow.cpp
@@ -978,7 +978,7 @@ void MainWindow::on_actionAbout_triggered()
     // QMessageBox mbox;
     // mbox.setText("About ddcutil");
 
-    QString copyright = "Copyright (C) 2014-2020 Sanford Rockowitz\n";
+    QString copyright = "Copyright (C) 2014-2021 Sanford Rockowitz\n";
     copyright = copyright + "Licensed under the terms of the GNU General Public License (GPL) ";
     copyright = copyright + "Version 2.0 or later.";
 
