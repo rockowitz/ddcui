4a25fa3f585dd065ba8442e8ec72563930c26f38
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2021-03-09 09:13:58 -0500

    (need to remove old tree from git)

--- src/old/imported/QtWaitingSpinner/waitingspinnerwidget.cpp
+++ src/old/imported/QtWaitingSpinner/waitingspinnerwidget.cpp
@@ -21,7 +21,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 // Own includes
-#include "waitingspinnerwidget.h"
+#include "../../old/imported/QtWaitingSpinner/waitingspinnerwidget.h"
 
 #include <cmath>
 #include <algorithm>
