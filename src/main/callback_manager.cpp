2e0f6f4912a66a949d73ded2101acaf8b9a73039
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2025-01-28 19:12:30 -0500

    postEventMsg(): handle recent Display_Status_Event changes

--- src/main/callback_manager.cpp
+++ src/main/callback_manager.cpp
@@ -1,6 +1,6 @@
 // callback_manager.cpp
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2025 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <glib-2.0/glib.h>
@@ -39,24 +39,38 @@ void postEventMsg(DDCA_Display_Status_Event evt) {
    char time_buf[40];
    create_timestamp(time_buf, 40);
    intmax_t thread_id = get_thread_id();
-  // printf("(%s) evt.dref=%p event_type=%d\n", __func__, evt.dref, evt.event_type);
+   bool debug = false;
+   // printf("(%s) evt.dref=%p event_type=%d\n", __func__, evt.dref, evt.event_type);
 
-  printf("[%s][%6jd](mainwindow.cpp/%s) Executing. dref=%s, event_type = %s\n",
+   if (debug)
+      printf("[%s][%6jd](mainwindow.cpp/%s) =====> Executing. dref=%s, event_type = %s\n",
         time_buf, thread_id, __func__, ddca_dref_repr(evt.dref), ddca_display_event_type_name(evt.event_type));
 
-   if (evt.event_type == DDCA_EVENT_DISPLAY_CONNECTED || evt.event_type == DDCA_EVENT_DISPLAY_DISCONNECTED) {
+   if (evt.event_type == DDCA_EVENT_DISPLAY_CONNECTED ||
+       evt.event_type == DDCA_EVENT_DISPLAY_DISCONNECTED ||
+       evt.event_type == DDCA_EVENT_DDC_ENABLED)
+   {
       QString qstitle("Display Status Change");
       QMessageBox::Icon icon = QMessageBox::Warning;
       QString qstext;;
       // QString qstext = (evt.event_type == DDCA_EVENT_DISPLAY_CONNECTED)
       //                    ?   QString("Display has been connected.   Redetect Displays")
       //                    :   QString("Display has been disconnected.  Redetect displays");
-      if (evt.event_type == DDCA_EVENT_DISPLAY_CONNECTED)
+      if (evt.event_type == DDCA_EVENT_DISPLAY_CONNECTED) {
+         if (evt.flags & DDCA_DISPLAY_EVENT_DDC_WORKING)
          qstext = QString("Display connected on %1, bus /dev/i2c-%2.")
                          .arg(evt.connector_name).arg(  evt.io_path.path.i2c_busno) ;
-       else {
+         else
+            qstext = QString("Display connected on %1, bus /dev/i2c-%2.\nDDC communication not working.")
+                            .arg(evt.connector_name).arg(  evt.io_path.path.i2c_busno) ;
+      }
+       else if (evt.event_type == DDCA_EVENT_DISPLAY_DISCONNECTED) {
          qstext = QString("Display disconnected on %1, bus /dev/i2c-%2.\n")
                          .arg(evt.connector_name).arg(evt.io_path.path.i2c_busno);
+       }
+       else {
+          qstext = QString("DDC communication became enabled on %1, bus /dev/i2c-%2.\n")
+                          .arg(evt.connector_name).arg(evt.io_path.path.i2c_busno);
        }
 
       MsgBoxQueueEntry* qe = new MsgBoxQueueEntry(qstitle, qstext, icon);
@@ -64,8 +78,9 @@ void postEventMsg(DDCA_Display_Status_Event evt) {
       // GlobalState::instance()._mainWindow->on_actionRedetect_triggered();  // creates new window
    }
   else {
-     printf("[%s][%6jd](mainwindow.cpp/%s) Ignoring event of type %s\n",
-           time_buf, thread_id, __func__, ddca_display_event_type_name(evt.event_type));
+     if (debug)
+        printf("[%s][%6jd](mainwindow.cpp/%s) Ignoring event of type %s\n",
+              time_buf, thread_id, __func__, ddca_display_event_type_name(evt.event_type));
   }
 }
 
@@ -77,7 +92,7 @@ CallbackManager::CallbackManager() {
 
 
 void CallbackManager::emitDisplayChanged(DDCA_Display_Status_Event evt) {
-   bool debug = true;
+   bool debug = false;
    TRACECF(debug, "Executing. event_type=%s, dref=%s",
            ddca_display_event_type_name(evt.event_type), ddca_dref_repr(evt.dref));
    emit displayChanged(evt);
@@ -85,7 +100,7 @@ void CallbackManager::emitDisplayChanged(DDCA_Display_Status_Event evt) {
 
 
 void display_status_event_callback(DDCA_Display_Status_Event evt) {
-   bool debug = false;
+   bool debug = true;
    char time_buf[40];
    create_timestamp(time_buf, 40);
    intmax_t thread_id = get_thread_id();
