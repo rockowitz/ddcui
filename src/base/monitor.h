700db53ef1233366fd3a437612156810f2640552
Sanford Rockowitz <rockowitz@minsoft.com>
2025-12-04 09:03:30 -0500

    add member variable _initChecksDone

--- src/base/monitor.h
+++ src/base/monitor.h
@@ -1,6 +1,6 @@
 /* monitor.h */
 
-// Copyright (C) 2018-2024 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2025 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef MONITOR_H
@@ -49,6 +49,7 @@ public:
     QString           dref_repr();
     void              dbgrpt();
 
+    bool                 _initChecksDone = false;
     const int            _monitorNumber = -1;    // 1 based
     DDCA_Display_Info2 * _displayInfo;
     FeatureBaseModel *   _baseModel;
