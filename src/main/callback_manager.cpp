f83a43a164192de60cfdc2dd2f742371e204ff30
Sanford Rockowitz <rockowitz@minsoft.com>
2025-12-05 01:48:53 -0500

    display_status_event_callback(): turn off debug

--- src/main/callback_manager.cpp
+++ src/main/callback_manager.cpp
@@ -105,7 +105,7 @@ void CallbackManager::emitDisplayChanged(DDCA_Display_Status_Event evt) {
 
 
 void display_status_event_callback(DDCA_Display_Status_Event evt) {
-   bool debug = true;
+   bool debug  = false;
    char time_buf[40];
    create_timestamp(time_buf, 40);
    intmax_t thread_id = get_thread_id();
