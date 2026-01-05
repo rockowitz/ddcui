7ae586cc26075f13b78e943b54f2abb28c585817
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-01-05 02:56:39 -0500

    sync utility dir with ddcutil

--- src/c_util/report_util.c
+++ src/c_util/report_util.c
@@ -516,20 +516,17 @@ void drpt_label(int depth, const char * text) {
  */
 void xvrpt_vstring(Byte opts, int depth, char* format, va_list ap) {
    // printf("(xvrpt_vstring) format=|%s|\n, ap=%p", format, ap);
-   int buffer_size = 200;
-   char buffer[buffer_size];
-   char * buf = buffer;
-   int reqd_size = vsnprintf(buffer, buffer_size, format, ap);
-   // if buffer wasn't sufficiently large, allocate a temporary buffer
-   if (reqd_size >= buffer_size) {
-      // printf("(%s) Allocating temp buffer, reqd_size=%d\n", __func__, reqd_size);
-      buf = malloc(reqd_size+1);
-      vsnprintf(buf, reqd_size+1, format, ap);
-   }
-   xrpt_label_collect(opts, depth, buf, NULL);
-   if (buf != buffer)
+   if (ap) {
+      char * buf = g_strdup_vprintf(format, ap);
+      xrpt_label_collect(opts, depth, buf, NULL);
       free(buf);
+   }
+   else {
+      xrpt_label_collect(opts, depth, format, NULL);
+      syslog(LOG_ERR, "(%s) NULL va_list, format=|%s|", __func__, format);
+   }
 }
+
 
 
 /** Writes a formatted string to the current output destination and/or
