6dbe00cca845e540ca6eab0360c3cb5100358e8f
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-05-13 15:54:53 -0400

    fix up includes

--- src/cmdline/ddcui_cmd_parser_aux.h
+++ src/cmdline/ddcui_cmd_parser_aux.h
@@ -1,15 +1,15 @@
-/** \file cmd_parser_aux.h
+/** \file ddcui_cmd_parser_aux.h
  *
  *  Functions and strings that are independent of the parser package used.
  */
 
-// Copyright (C) 20014-2022 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2014-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef DDCUI_CMD_PARSER_AUX_H_
 #define DDCUI_CMD_PARSER_AUX_H_
 
-#include <string.h>
+#include <stdbool.h>
 
 #include "ddcui_parsed_cmd.h"
 
