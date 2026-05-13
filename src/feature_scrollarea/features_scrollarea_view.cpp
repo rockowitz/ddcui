8003e2c0c448eb91c9a5e730b5bc10421e57640c
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-05-13 17:29:04 -0400

    clean up the mess that eclipse or claude made of the includes

--- src/feature_scrollarea/features_scrollarea_view.cpp
+++ src/feature_scrollarea/features_scrollarea_view.cpp
@@ -1,6 +1,6 @@
 /* features_scrollarea_view.cpp */
 
-// Copyright (C) 2018-2020 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "feature_scrollarea/features_scrollarea_view.h"
