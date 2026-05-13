f88fed12096e3f7fc6c542c8d7aec6141f7a32bd
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-05-13 14:25:44 -0400

    delete unneeded include

--- src/base/ddcui_core.h
+++ src/base/ddcui_core.h
@@ -1,6 +1,6 @@
 /** @file ddcui_core.h */
 
-// Copyright (C) 2018-2023 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef DDCUI_CORE_H
