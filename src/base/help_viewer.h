91f14edb26beefef40cd1c09debc031909b0a599
Sanford Rockowitz (/shared/home/rock/dot_gitconfig) <rockowitz@minsoft.com>
2026-05-13 15:29:16 -0400

    doxydoc header, whitespace

--- src/base/help_viewer.h
+++ src/base/help_viewer.h
@@ -1,22 +1,18 @@
-// help_viewer.c
+/** @file help_viewer.h */
 
-// Copyright (C) 2018 Sanford Rockowitz <rockowitz@minsoft.com>
+// Copyright (C) 2018-2026 Sanford Rockowitz <rockowitz@minsoft.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-
-
-#ifndef HELP_VIEWER_C_
-#define HELP_VIEWER_C_
+#ifndef HELP_VIEWER_H_
+#define HELP_VIEWER_H_
 
 #include <QtCore/QtCore>
 #include <QtWidgets/QDialog>
 
 class QTextBrowser;
 
-
 namespace HelpViewer {
 void show(QString helpfile, QString title);
 };
 
-
-#endif /* HELP_VIEWER_C_ */
+#endif /* HELP_VIEWER_H_ */
